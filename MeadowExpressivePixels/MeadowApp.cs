014d5b6e6ae513e78c9ba3947c6b227889fb9a06
Mark A Jones <Mark.Alan.Jones@outlook.com>
2021-06-28 11:45:25 -0400

    upgrade to b5.0

--- MeadowExpressivePixels/MeadowApp.cs
+++ MeadowExpressivePixels/MeadowApp.cs
@@ -1,6 +1,6 @@
 ﻿using Meadow;
 using Meadow.Devices;
-using Meadow.Foundation.Displays.Tft;
+using Meadow.Foundation.Displays.TftSpi;
 using Meadow.Foundation.Graphics;
 using Meadow.Hardware;
 using Microsoft.ExpressivePixels;
